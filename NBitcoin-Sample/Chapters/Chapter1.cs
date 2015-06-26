3ddbae40396d68591c326c30ad460d1b27bf9317
abaziz <aziz@aurorasolutions.io>
2015-06-26 17:07:12 +0500

    Added Chapter 1. Lesson 1 and Lesson 2
    - How to generate new keys, use public key, bitcoin address and script
    pub key

---
+++ NBitcoin-Sample/Chapters/Chapter1.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using NBitcoin;
+
+namespace NBitcoin_Sample.Chapters
+{
+    class Chapter1
+    {
+
+        public void Lesson1()
+        {
+            Key key = new Key(); //generates a new private key.     
+            PubKey pubKey = key.PubKey; //gets the matching public key.     
+            Console.WriteLine("Public Key: {0}", pubKey);
+            KeyId hash = pubKey.Hash; //gets a hash of the public key.     
+            Console.WriteLine("Hashed public key: {0}", hash);
+            BitcoinAddress address = pubKey.GetAddress(Network.Main); //retrieves the bitcoin address.     
+            Console.WriteLine("Address: {0}", address);
+            Script scriptPubKeyFromAddress = address.ScriptPubKey;
+            Console.WriteLine("ScriptPubKey from address: {0}", scriptPubKeyFromAddress);
+            Script scriptPubKeyFromHash = hash.ScriptPubKey;
+            Console.WriteLine("ScriptPubKey from hash: {0}", scriptPubKeyFromHash);
+
+        }
+
+        public void Lesson2()
+        {
+            Script scriptPubKey = new Script("OP_DUP OP_HASH160 1b2da6ee52ac5cd5e96d2964f12a0241851f8d2a OP_EQUALVERIFY OP_CHECKSIG");
+            BitcoinAddress address = scriptPubKey.GetDestinationAddress(Network.Main);
+            Console.WriteLine("Bitcoin Address: {0}", address);
+        }
+    }
+}
--- NBitcoin-Sample/Program.cs
+++ NBitcoin-Sample/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using NBitcoin_Sample.Chapters;
 
 namespace NBitcoin_Sample
 {
@@ -10,6 +11,10 @@ namespace NBitcoin_Sample
     {
         static void Main(string[] args)
         {
+            Chapter1 chapter = new Chapter1();
+            chapter.Lesson1();
+            Console.WriteLine("\n\n\nPress enter to continue.");
+            Console.ReadLine(); 
         }
     }
 }
